be0dc938e4a7526ffd9dcd003f6558c7e985cb8e
shadhk <shadhussaink@gmail.com>
2022-01-05 22:42:42 +0530

    Serverside validation added to the ticket creation and reply message on the ticket

--- src/middlewares/formValidation.middleware.js
+++ src/middlewares/formValidation.middleware.js
@@ -6,6 +6,9 @@ const pin = Joi.number().integer().min(000000).max(999999).required()
 
 const newPassword = Joi.string().alphanum().min(3).max(30).required()
 
+const shortStr = Joi.string().min(3).max(50)
+const longStr = Joi.string().min(3).max(50)
+
 const resetPasswordReqValidation = (req, res, next) => {
   const schema = Joi.object({ email })
 
@@ -28,7 +31,38 @@ const updatePasswordReqValidation = (req, res, next) => {
   next()
 }
 
+const createNewTicketValidation = (req, res, next) => {
+  const schema = Joi.object({
+    subject: shortStr.required(),
+    sender: shortStr.required(),
+    message: longStr.required()
+  })
+
+  const value = schema.validate(req.body)
+
+  if (value.error) {
+    return res.json({ status: "error", message: value.error.message })
+  }
+  next()
+}
+
+const replyTicketMessageValidation = (req, res, next) => {
+  const schema = Joi.object({
+    sender: shortStr.required(),
+    message: longStr.required()
+  })
+
+  const value = schema.validate(req.body)
+
+  if (value.error) {
+    return res.json({ status: "error", message: value.error.message })
+  }
+  next()
+}
+
 module.exports = {
   resetPasswordReqValidation,
-  updatePasswordReqValidation
+  updatePasswordReqValidation,
+  createNewTicketValidation,
+  replyTicketMessageValidation
 }
--- src/routers/ticket.router.js
+++ src/routers/ticket.router.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = express.Router()
 const { insertTicket, getTickets, getTicketById, updateClientReply, updateStatusClose, deleteTicket } = require("../model/ticket/Ticket.Model")
 const { userAuthorization } = require("../middlewares/authorization.middleware")
+const { createNewTicketValidation, replyTicketMessageValidation } = require("../middlewares/formValidation.middleware")
 
 router.all("/", (req, res, next) => {
   // res.json({ message: "return from ticket router" })
@@ -10,7 +11,7 @@ router.all("/", (req, res, next) => {
 })
 
 //create new ticket
-router.post("/", userAuthorization, async (req, res) => {
+router.post("/", createNewTicketValidation, userAuthorization, async (req, res) => {
   try {
     const { subject, sender, message } = req.body
     const user_id = req.user_id
@@ -61,7 +62,7 @@ router.get("/:_id", userAuthorization, async (req, res) => {
 })
 
 //update reply message from client
-router.put("/:_id", userAuthorization, async (req, res) => {
+router.put("/:_id", replyTicketMessageValidation, userAuthorization, async (req, res) => {
   try {
     const { message, sender } = req.body
     const { _id } = req.params
